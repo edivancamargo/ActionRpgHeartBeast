d7939c7f00433dc63cadcc470c1de849644a4371
Edivan Camargo <camargo.edivan@gmail.com>
2022-04-27 22:45:20 +0100

    Add Hitbox and Hurtbox collisions. Sword animation working with collisions.

--- src/World/GrassScene.gd
+++ src/World/GrassScene.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 var GrassEffect: Resource = preload("res://src/Effects/GrassEffect.tscn")
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack_action"):
-		var grassEffect: Node2D = GrassEffect.instance() as Node2D
-		grassEffect.position = self.position
-		get_parent().add_child(grassEffect)
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
 
-		queue_free()
+func create_grass_effect():
+	var grassEffect: Node2D = GrassEffect.instance() as Node2D
+	grassEffect.position = self.position
+	get_parent().add_child(grassEffect)
