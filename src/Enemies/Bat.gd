7fa457d943f1a187adf750d0f148de5ec6c59927
Edivan Camargo <camargo.edivan@gmail.com>
2022-04-28 22:56:55 +0100

    Add Hurtbox and knockback to the Bat

--- src/Enemies/Bat.gd
+++ src/Enemies/Bat.gd
@@ -1,7 +1,13 @@
 extends KinematicBody2D
 
-func _ready():
-	pass # Replace with function body.
+var knockback = Vector2.ZERO
+
+func _physics_process(delta):
+	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = move_and_slide(knockback)
 
 func _on_Hurtbox_area_entered(area):
-	pass # Replace with function body.
+	# area in this case is SwordHitbox.gd (Area2D), so, it has knockback_vector defined over there and
+	# we are updating its value in the PLayer.gd
+	# I don't like this solution though...
+	knockback = area.knockback_vector * 120
