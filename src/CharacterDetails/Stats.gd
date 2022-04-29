c9f3405b507cbdab996a6f02bdd69693d397e8c6
Edivan Camargo <camargo.edivan@gmail.com>
2022-04-29 09:05:00 +0100

    print damage for reference

--- src/CharacterDetails/Stats.gd
+++ src/CharacterDetails/Stats.gd
@@ -6,6 +6,7 @@ onready var health = max_health setget set_damage
 signal no_health
 
 func set_damage(damage: int):
+	print("set_damage: " + String(damage))
 	health = health - damage
 	if health <= 0:
 		emit_signal("no_health")
