12b3968029a29ceb55cd04e32d65b2a2f3727c7a
Edivan Camargo <camargo.edivan@gmail.com>
2022-04-29 08:50:37 +0100

    Stats emit signal when no_health. Enemy listens the signal and queue_free for now.

--- src/CharacterDetails/Stats.gd
+++ src/CharacterDetails/Stats.gd
@@ -1,5 +1,11 @@
 extends Node
 
 export(int) var max_health = 1
-var health = max_health
+onready var health = max_health setget set_damage
 
+signal no_health
+
+func set_damage(damage: int):
+	health = health - damage
+	if health <= 0:
+		emit_signal("no_health")
--- src/Enemies/Bat.gd
+++ src/Enemies/Bat.gd
@@ -4,12 +4,22 @@ onready var stats = $Stats
 
 var knockback = Vector2.ZERO
 
+func _ready():
+	print(stats.max_health)
+	print(stats.health)
+
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
 
 func _on_Hurtbox_area_entered(area):
+	stats.set_damage(1)
+	print(stats.health)
 	# area in this case is SwordHitbox.gd (Area2D), so, it has knockback_vector defined over there and
 	# we are updating its value in the PLayer.gd
 	# I don't like this solution though...
 	knockback = area.knockback_vector * 120
+
+func _on_Stats_no_health():
+	print("no_health listened")
+	queue_free()
