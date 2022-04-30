c58633ea025f4a81a82563950e0dd3373c8a7e5f
Edivan Camargo <camargo.edivan@gmail.com>
2022-04-30 17:31:10 +0100

    Export offset variable in the Hurtbox script to be able to position the animation accordingly to the parent position

--- src/Collisions/Hurtbox.gd
+++ src/Collisions/Hurtbox.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
 export(bool) var show_hit_effect = true
+export(Vector2) var offset = Vector2.ZERO
 
 const HitEffect = preload("res://src/Effects/HitEffect.tscn")
 
 func _on_Hurtbox_area_entered(area):
 	if show_hit_effect:
-		HitEffect.instance().attach(self)
+		HitEffect.instance().attach(self, offset)
