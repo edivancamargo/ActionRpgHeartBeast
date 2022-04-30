fa0feca24ea0104b246976dbb95a434d94529090
Edivan Camargo <camargo.edivan@gmail.com>
2022-04-30 15:45:44 +0100

    Hurtbox shows HitEffect animation by default with option to turn it off from the parent node.

--- src/Collisions/Hurtbox.gd
+++ src/Collisions/Hurtbox.gd
@@ -1,16 +1,9 @@
 extends Area2D
 
+export(bool) var show_hit_effect = true
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const HitEffect = preload("res://src/Effects/HitEffect.tscn")
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Hurtbox_area_entered(area):
+	if show_hit_effect:
+		HitEffect.instance().attach(self)
