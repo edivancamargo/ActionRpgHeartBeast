f4a7f0cfb6bc5e249268e1b7c53f51ba3f2f6237
Edivan Camargo <camargo.edivan@gmail.com>
2022-04-30 15:48:56 +0100

    Tidying up Bat script

--- src/Enemies/Bat.gd
+++ src/Enemies/Bat.gd
@@ -6,17 +6,12 @@ onready var stats = $Stats
 
 var knockback = Vector2.ZERO
 
-func _ready():
-	print(stats.max_health)
-	print(stats.health)
-
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
 
 func _on_Hurtbox_area_entered(area):
 	stats.set_damage(area.damage)
-	print(stats.health)
 	# area in this case is SwordHitbox.gd (Area2D), so, it has knockback_vector defined over there and
 	# we are updating its value in the PLayer.gd
 	# I don't like this solution though...
