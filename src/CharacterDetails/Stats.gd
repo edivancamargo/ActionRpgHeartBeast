aeafac35933fa4b5bde7b5de7056f3c229f3c03a
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-01 11:55:20 +0100

    Refactor Stats.gd to not allow setting the health from outside.

--- src/CharacterDetails/Stats.gd
+++ src/CharacterDetails/Stats.gd
@@ -1,11 +1,16 @@
 extends Node
 
 export(int) var max_health = 1
-onready var health = max_health setget set_damage
+onready var health: int = max_health setget set_health
 
 signal no_health
 
+func set_health(value) -> void:
+	push_error("PRIVATE PROPERTY: Health should not be setted from outside")
+
 func set_damage(damage: int):
+	print("set_damage: " + String(damage))
 	health = health - damage
+	print ("health: " + String(health))
 	if health <= 0:
 		emit_signal("no_health")
