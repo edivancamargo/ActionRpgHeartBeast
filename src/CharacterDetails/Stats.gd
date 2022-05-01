3b8c41e88a1ac6461f97937d2f82e351d576c81d
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-01 13:34:42 +0100

    Update the HeartsUI with the correct values from player

--- src/CharacterDetails/Stats.gd
+++ src/CharacterDetails/Stats.gd
@@ -1,16 +1,24 @@
 extends Node
 
-export(int) var max_health = 1
+export(int) var max_health = 1 setget set_max_health
 onready var health: int = max_health setget set_health
 
 signal no_health
+signal health_changed(value)
+signal max_health_changed(value)
 
-func set_health(value) -> void:
-	push_error("PRIVATE PROPERTY: Health should not be setted from outside")
+func set_health(value: int) -> void:
+	health = min(value, max_health)
+
+func set_max_health(value: int) -> void:
+	max_health = value
+	set_health(value)
+	emit_signal("max_health_changed", max_health)
 
 func set_damage(damage: int):
 	print("set_damage: " + String(damage))
-	health = health - damage
-	print ("health: " + String(health))
-	if health <= 0:
+	self.health = self.health - damage
+	print ("health: " + String(self.health))
+	emit_signal("health_changed", self.health)
+	if self.health <= 0:
 		emit_signal("no_health")
