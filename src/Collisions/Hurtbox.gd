b33555c4af33972d6f65160bf59fae30ad381edc
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-01 11:59:49 +0100

    Create a period of invencibility after being hit by a enemy.

--- src/Collisions/Hurtbox.gd
+++ src/Collisions/Hurtbox.gd
@@ -1,10 +1,37 @@
 extends Area2D
 
-export(bool) var show_hit_effect = true
-export(Vector2) var offset = Vector2.ZERO
-
 const HitEffect = preload("res://src/Effects/HitEffect.tscn")
 
-func _on_Hurtbox_area_entered(area):
-	if show_hit_effect:
-		HitEffect.instance().attach(self, offset)
+export(Vector2) var offset = Vector2.ZERO
+
+signal invincibility_started
+signal invincibility_ended
+
+onready var timer:Timer = $Timer
+var invincible: bool = false setget set_invincible
+
+func set_invincible(value: bool) -> void:
+	invincible = value
+	if invincible:
+		emit_signal("invincibility_started")
+	else:
+		emit_signal("invincibility_ended")
+
+func start_invincibility(duration: float) -> void:
+	self.set_invincible(true)
+	timer.start(duration)
+
+func create_hit_effect() -> void:
+	HitEffect.instance().attach(self, offset)
+
+func _on_Timer_timeout():
+	print("_on_Timer_timeout")
+	self.set_invincible(false)
+
+# CacGyver solution for enemies entering the hurtbox and not leaving it.
+# Below we are disabling monitoring for the collision layers for the duration of the timer...
+func _on_Hurtbox_invincibility_started():
+	set_deferred("monitoring", false)
+
+func _on_Hurtbox_invincibility_ended():
+	monitoring = true
