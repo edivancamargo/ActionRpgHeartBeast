075264600ad8c8c28198045989f5a36e4ec0e9f9
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-02 01:15:48 +0100

    Bats now will fly around when world scene is ready and do not stay at their initial position indefinitely

--- src/Enemies/Bat.gd
+++ src/Enemies/Bat.gd
@@ -19,13 +19,13 @@ onready var playerDetectionZone = $PlayerDetectionZone
 onready var sprite = $AnimatedSprite
 onready var batHurtbox = $Hurtbox
 onready var softCollision = $SoftCollision
+onready var wanderController = $WanderController
 
 var knockback = Vector2.ZERO
 var velocity = Vector2.ZERO
-var initial_position: Vector2
 
 func _ready():
-	initial_position = self.global_position
+	state = pick_random_state([IDLE, WANDER])
 
 func _physics_process(delta) -> void:
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -33,33 +33,51 @@ func _physics_process(delta) -> void:
 	
 	match state:
 		IDLE:
-			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta) #Friction
-			seek_player(delta)
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+			set_random_state()
 		WANDER:
-			pass
+			wander_state(delta)
 		CHASE:
 			chase_player(delta)
-			sprite.flip_h = velocity.x < 0
 	
 	if softCollision.is_colliding():
 		velocity += softCollision.get_push_vector() * delta * 200
 	velocity = move_and_slide(velocity)
 
-func seek_player(delta) -> void:
+func wander_state(delta: float) -> void:
+	seek_player()
+	set_random_state()
+	accelerate_toward_point(delta, wanderController.target_position)
+	var tolerance = MAX_SPEED * delta
+	if global_position.distance_to(wanderController.target_position) <= tolerance:
+		set_random_state()
+
+func accelerate_toward_point(delta: float, point: Vector2) -> void:
+#	var direction = (player.global_position - global_position).normalized()
+	var direction = global_position.direction_to(point)
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	sprite.flip_h = velocity.x < 0
+
+func set_random_state() -> void:
+	if wanderController.get_time_left() == 0:
+		state = pick_random_state([IDLE, WANDER])
+		wanderController.start_wander_timer(rand_range(1,3))
+
+func seek_player() -> void:
 	if playerDetectionZone.can_see_player():
 		state = CHASE
 
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
+
 func chase_player(delta) -> void:
-	var player = playerDetectionZone.player
+	var player = playerDetectionZone.player as KinematicBody2D
 	if player != null:
-		var direction = (player.global_position - global_position).normalized()
-		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+		accelerate_toward_point(delta, player.global_position)
 	else:
 		state = IDLE
-
-func return_to_initial_position() -> void:
-	print("returning to initial position")
-	pass
 
 func _on_Hurtbox_area_entered(area) -> void:
 	stats.set_damage(area.damage)
@@ -82,4 +100,3 @@ func _on_PlayerDetectionZone_body_entered_detection_zone():
 
 func _on_PlayerDetectionZone_body_exited_detection_zone():
 	print("listened body_exited_detection_zone")
-	return_to_initial_position()
