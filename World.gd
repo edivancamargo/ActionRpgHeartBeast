22bacde898c2a646e94fae31ef9a70047d52a386
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-02 11:30:57 +0100

    Adds a music to the world scene. Play it when the World is ready.

--- World.gd
+++ World.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
+onready var worldMusicPlayer = $WorldMusicPlayer
+
 func _ready():
 #	Games uses seeds to generate random numbers. This means, if we don't call
 #	randomize the result of random generators will always be the same...
 	randomize()
+	worldMusicPlayer.play()
+	
