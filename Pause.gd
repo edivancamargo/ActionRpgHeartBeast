a2c9d88d24d0971e7d4ccb3f34663fe6e6c48b52
Edivan Camargo <camargo.edivan@gmail.com>
2022-05-08 18:14:34 +0100

    Add pause menu screen.

--- Pause.gd
+++ Pause.gd
@@ -2,7 +2,16 @@ extends Control
 
 func _input(event):
 	if event.is_action_pressed("pause"):
-		var tree = get_tree()
-		var paused = not tree.paused
-		tree.paused = paused
-		visible = paused
+		set_pause()
+
+func _on_ResumeBtn_pressed():
+	set_pause()
+
+func set_pause() -> void:
+	var tree = get_tree()
+	var paused = not tree.paused
+	tree.paused = paused
+	visible = paused
+
+func _on_QuitBtn_pressed():
+	get_tree().quit()
